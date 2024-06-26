1d73b648206ced8f03c9f492be67a7e51c9dbafc
phiebs02 <phiebs.d@gmail.com>
2024-06-26 19:46:56 +0200

    laatste aanpassingen api

--- app/Http/Controllers/Api/ApiWoordController.php
+++ app/Http/Controllers/Api/ApiWoordController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Woord;
 use Illuminate\Http\Request;
-
 /**
  * @OA\Info(
  *     version="1.0.0",
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiThemaController;
-use App\Http\Controllers\ApiWoordController;
+use App\Http\Controllers\Api\ApiThemaController;
+use App\Http\Controllers\Api\ApiWoordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,11 +18,16 @@ use App\Http\Controllers\ApiWoordController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
 
-    Route::get('/woorden', [ApiWoordController::class, 'index']);
+// Route::middleware(['auth:sanctum'])->group(function () {
+
+
+
+// });
+Route::get('/woorden', [ApiWoordController::class, 'index']);
 Route::get('/woorden/{id}', [ApiWoordController::class, 'show']);
 Route::get('/woorden/thema/{themaId}', [ApiWoordController::class, 'getWordsByThema']);
 
 Route::get('/themas', [ApiThemaController::class, 'index']);
 Route::get('/themas/{id}', [ApiThemaController::class, 'show']);
-});
