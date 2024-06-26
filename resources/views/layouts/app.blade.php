4184ecb3bb323869856c22e3ff95320ab509e705
phiebs02 <phiebs.d@gmail.com>
2024-06-26 13:20:41 +0200

    https issue 2.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://roadtrip-8twkp.ondigitalocean.app/build/assets/app-BHKf-uCm.css">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
